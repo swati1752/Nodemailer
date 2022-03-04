44d2752a313a5d35b66464cec72fb0af838f8070
swati1752 <swatibansal1752@gmail.com>
2022-03-04 19:46:03 +0530

    adding cc and bcc

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config()
 async function mail(){
-let testAccount = await nodemailer.createTestAccount();
 
-// create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
+
+const transporter = nodemailer.createTransport({
   host: "smtp.ethereal.email",
   port: 587,
-  secure: false, // true for 465, false for other ports
+  secure: false, 
   service:'gmail',
   auth: {
-    user: process.env.SENDER_EMAIL, // generated ethereal user
-    pass: process.env.SENDER_PASS, // generated ethereal password
+    user: process.env.SENDER_EMAIL, 
+    pass: process.env.SENDER_PASS, 
   },
   tls : {
       rejectUnauthorized:false
   }
 });
 
-let info = await transporter.sendMail({
+const info = await transporter.sendMail({
     from: "mail.Netflix", // sender address
     to: process.env.RECEIVE_MAIL.split(','), // list of receivers
-    subject: "Netflix-originals", // Subject line
-    text: "team conatus", // plain text body
+    cc:process.env.SENDER_EMAIL,
+    bcc:process.env.SENDER_EMAIL,
+    subject: "Netflix-originals", 
+    text: "team conatus", 
     html: "<i>https://geez.art.blog/2021/10/18/okay-but-what/</i><br><b>here is the link to upcoming Netflix-originals script . . . . THE PEN</b> <p>written by Manu Shukla </p>", // html body
   });
 }
